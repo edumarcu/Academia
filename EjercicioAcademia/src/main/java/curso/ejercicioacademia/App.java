5ce47babba2e6d2b1f08eb533609ba06dd67adc7
Jesús Aísa Vicén <jesusaisa82@gmail.com>
2013-01-18 13:24:28 +0100

    Commit 2

--- EjercicioAcademia/src/main/java/curso/ejercicioacademia/App.java
+++ EjercicioAcademia/src/main/java/curso/ejercicioacademia/App.java
@@ -1,13 +1,9 @@
 package curso.ejercicioacademia;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Academia" );
     }
 }
