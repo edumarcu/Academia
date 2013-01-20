cc89e0f0e1fae6a51a225e14eca2b0e804359fe9
Eduardo <edumarcu@gmail.com>
2013-01-20 15:12:59 +0100

    print Academia 2

--- EjercicioAcademia/src/main/java/curso/ejercicioacademia/application/App.java
+++ EjercicioAcademia/src/main/java/curso/ejercicioacademia/application/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main( String[] args ) {
         Persona p = new Alumno ("Edu");
         
-        System.out.println("Academia");
+        System.out.println("Academia de Edu y Jesus");
         System.out.println(p);
     }
 }
