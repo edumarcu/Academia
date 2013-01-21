621dcf9635d04d348a65052dc361d35a97dd7bc2
Jesús Aísa Vicén <jesusaisa82@gmail.com>
2013-01-21 12:21:24 +0100

    Merge

--- EjercicioAcademia/src/main/java/curso/ejercicioacademia/application/App.java
+++ EjercicioAcademia/src/main/java/curso/ejercicioacademia/application/App.java
@@ -132,6 +132,7 @@ public class App {
         System.out.println(aulas[1].getAsignaturas());
         System.out.println("");
 
+
     }
     
     public static String printAsignaturas(Academia academia){
